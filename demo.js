3c34b46605a93a8b45b18ac591b0802ce8b16d2b
jousethen <jthen93@gmail.com>
2021-07-29 14:41:13 -0400

    Lab complete

--- demo.js
+++ demo.js
@@ -23,7 +23,7 @@ function likeCallback(e) {
   mimicServerCall()
     .then(function (serverMessage) {
       // STEP 2: Uncomment the next 3 lines.
-      //== We'll use Pillar 1 (DOM Manipulation) to update the screen and
+      // We'll use Pillar 1 (DOM Manipulation) to update the screen and
       // mimic Pillar 3 (Server Communication) to only update the screen if the
       // sending of information to the server succeeds.
       alert("You notified the server!");
